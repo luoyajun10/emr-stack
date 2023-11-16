450786313ab0eb626fba327e1204cba5397ad5cf
luoyajun10 <luoyajun1010@gmail.com>
2023-11-16 12:37:49 +0800

    [Fix] Update spark on hive engine to mr for spark sql

--- service/spark/script/deploy.sh
+++ service/spark/script/deploy.sh
@@ -3,7 +3,7 @@
 # todo: copy conf files
 
 # spark on hive
-cd /usr/lib/spark-current/conf
-ln -s -f /usr/lib/hive-current/conf/hive-site.xml hive-site.xml
+cp /usr/lib/hive-current/conf/hive-site.xml /usr/lib/spark-current/conf
+sed -i 's;<value>tez<\/value>;<value>mr<\/value>;g' /usr/lib/spark-current/conf/hive-site.xml
 cd /usr/lib/spark-current/jars
 ln -s -f /usr/lib/hive-current/lib/mysql-connector-java-5.1.31-bin.jar mysql-connector-java-5.1.31.jar
